a7dcac1058db4f8028b875feefe75f34e45cd1be
Himesh-Bhattarai <himesh.hcb@example.com>
2025-06-09 07:26:43 +0545

    Classwork

--- 404_Teach/Classwork/AreaOfRectangle.c
+++ 404_Teach/Classwork/AreaOfRectangle.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+void area(int length, int breadth);//Decleration of Functuon 
 int main(){
+int l,b;
 
+
+//call the functions
+area(l,b);
+return 0;
+}
+
+//function 
+void areaORectangle(int length, int breadth){
+    printf("Area of rectangle is %d \n", length * breadth)
 }
